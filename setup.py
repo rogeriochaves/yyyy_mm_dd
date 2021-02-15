8062c29d56882845045bf1d7573f3adce439106e
Rogerio Chaves <rogeriocfj@gmail.com>
2021-02-15 12:18:20 +0100

    Bump to v0.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
     name='yyyy_mm_dd',
     packages=find_packages(include=['yyyy_mm_dd']),
-    version='0.1.0',
+    version='0.1.1',
     description='Helper functions for easy string-based manipulation of dates in python',
     long_description=long_description,
     long_description_content_type="text/markdown",
